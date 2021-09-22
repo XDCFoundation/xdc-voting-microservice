c70020fd98ecc7d400a143864132087acf7de7b3
Sushmita <sushmita@leewayhertz.com>
2021-09-22 19:10:04 +0530

    microservice

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,17 +1,25 @@
 import Config from '.'
 import mongoose from 'mongoose'
+const fs = require("fs");
 
 export default class DBConnection {
   static connect () {
     console.log('DB trying to connect on ' + new Date() + ' to url' + Config.DB)
-
+    const caContent = [
+      fs.readFileSync(__dirname + "/rds-combined-ca-bundle.pem"),
+    ];
     const options = {
       keepAlive: 1,
       autoReconnect: true,
       poolSize: 10,
+      ssl: true,
+      sslValidate: false,
+      sslCA: caContent,
       useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      retryWrites: false
+    };
     return mongoose.connect(Config.DB, options)
   }
 }
