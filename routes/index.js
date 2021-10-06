2fb75a64aa0d5e6bf265a1e9d1fa697cd54d20ad
deepali <deepali@leewayhertz.com>
2021-10-06 19:54:15 +0530

    castvotebyproposalapi

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ import * as ValidationManger from "../middleware/validation";
 import { stringConstants } from "../app/common/constants";
 import Address from "../app/modules/address";
 import Proposal from "../app/modules/proposal";
+import votes from "../app/models/votes";
 
 module.exports = (app) => {
   app.get("/", (req, res) => res.send(stringConstants.SERVICE_STATUS_HTML));
@@ -20,7 +21,11 @@ module.exports = (app) => {
   /**
    * route definition
    */
-  app.post("/add-proposal", ValidationManger.validateAddProposal, new Proposal().addProposal);
+  app.post(
+    "/add-proposal",
+    ValidationManger.validateAddProposal,
+    new Proposal().addProposal
+  );
   app.get("/getProposalList", new Proposal().getProposalList);
   app.get("/getVotersListByProposal", new Proposal().getVotersListByProposal);
   app.get("/getProposalByDate", new Proposal().getProposalByDate);
@@ -29,9 +34,13 @@ module.exports = (app) => {
   app.get("/getPassedProposal", new Proposal().getPassedProposal);
   app.get("/getTotalProposalList", new Proposal().getTotalProposalList);
   app.get("/getPaginatedProposalList", new Proposal().getPaginatedProposalList);
-  app.get("/getListOfAddress",new Proposal().getListOfAddress);
-  app.get("/getListOfWhitelistedAddress",new Proposal().getListOfWhitelistedAddress);
-  app.get("/getSingleProposalDetail",new Proposal().getSingleProposalDetail);
+  app.get("/getListOfAddress", new Proposal().getListOfAddress);
+  app.get(
+    "/getListOfWhitelistedAddress",
+    new Proposal().getListOfWhitelistedAddress
+  );
+  app.get("/getSingleProposalDetail", new Proposal().getSingleProposalDetail);
+  app.post("/castProposalVote", new Address().castProposalVote);
 
   // app.get("/test-route", ValidationManger.validateUserLogin, new TestModule().testRoute);
 };
